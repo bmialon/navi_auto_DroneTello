0dda4a0ea280772744f57ede3a93ae3e3c40a28d
BastienLAFONT <148950652+BastienLAFONT@users.noreply.github.com>
2025-03-25 09:45:01 +0100

    Add files via upload

---
+++ Python/installer.bat
@@ -0,0 +1,17 @@
+@echo off
+echo ====================================
+echo  INSTALLATION DES LIBRAIRIES PYTHON
+echo ====================================
+echo.
+
+:: Installation des librairies nécessaires
+pip install djitellopy
+pip install opencv-python
+pip install numpy
+pip install mediapipe
+pip install pygame
+pip install keyboard
+
+echo.
+echo === INSTALLATION TERMINEE ===
+pause
